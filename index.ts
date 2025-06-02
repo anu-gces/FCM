3cc1c416f505d95c603465349eccda08beedfb85
anu-gces <be2020ce57@gces.edu.np>
2025-06-02 20:27:28 +0545

    second commit

--- index.ts
+++ index.ts
@@ -1,9 +1,25 @@
 import express from "express";
 import admin from "firebase-admin";
+import { config } from "dotenv";
+
 //@ts-ignore
 import cors from "cors";
 
-const serviceAccount = require("./siru-res-pos-firebase-adminsdk-7wlis-699fcf38fd.json");
+config(); // Load environment variables from .env file
+
+const serviceAccount = {
+  type: process.env.TYPE,
+  project_id: process.env.PROJECT_ID,
+  private_key_id: process.env.PRIVATE_KEY_ID,
+  private_key: process.env.PRIVATE_KEY?.replace(/\\n/g, "\n"),
+  client_email: process.env.CLIENT_EMAIL,
+  client_id: process.env.CLIENT_ID,
+  auth_uri: process.env.AUTH_URI,
+  token_uri: process.env.TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
+  universe_domain: process.env.UNIVERSE_DOMAIN,
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),

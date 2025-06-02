7b9aa30859d34e511a2ce5b9832699961c404999
anu-gces <153482121+anu-gces@users.noreply.github.com>
2025-06-02 20:53:44 +0545

    Update index.ts

--- index.ts
+++ index.ts
@@ -44,7 +44,7 @@ app.post("/send-fcm", async (req, res) => {
     const response = await admin.messaging().sendEach(
       tokens.map((token) => ({
         token,
-        body: {
+        data: {
           title: "New Order!",
           body: "You have a new order in CakeCafe!",
         },
